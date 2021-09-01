d3e0354301356344e9a25e2f2184007fcaa58cf3
Song-ki-beom <63495505+Song-ki-beom@users.noreply.github.com>
2021-09-02 00:31:29 +0900

    Revert "온갖 웨이브 관련 버그 수정"
    This reverts commit 6bc5a7ec78e992a2dc3bce2723df8f1bf6f4a068.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class Inventory : MonoBehaviour
+{
+    private int MAX_INVENTORY_CAPACITY = 5; // 임의로 5로 지정해두겠습니다.
+    public TowerManager _towerManager;
+    public List<Toggle> _toggle;
+    public List<GameObject> _tower;
+
+    [SerializeField] GameObject _toggleGrid;
+    [SerializeField] GameObject _togglePrefab;
+
+
+    private void Start()
+    {
+        OffToggle();
+    }
+
+    private void OffToggle()
+    {
+        for (int i = 0; i < _toggle.Count; i++)
+        {
+            _toggle[i].isOn = false;
+        }
+    }
+
+    public GameObject GetSelectedTower()
+    {
+        return _tower[GetActiveToggleIndex()];
+    }
+
+    private int GetActiveToggleIndex()
+    {
+        for (int i = 0; i < _toggle.Count; i++)
+        {
+            if (_toggle[i].isOn)
+                return i;
+        }
+        return -1;
+    }
+
+    public void AddItem(GameObject tower)
+    {
+        GameObject toggle = Instantiate(_togglePrefab);
+        toggle.transform.SetParent(_toggleGrid.transform, false);
+        _toggle.Add(toggle.GetComponent<Toggle>());
+        SetToggleContents(toggle, tower);
+
+        _tower.Add(tower);
+    }
+
+    private void SetToggleContents(GameObject toggle, GameObject tower)
+    {
+        // TODO: 인벤토리 버튼 내용 꾸미기
+        Toggle toggleComponent = toggle.GetComponent<Toggle>();
+        toggleComponent.group = _toggleGrid.GetComponent<ToggleGroup>();
+        Text label = toggle.GetComponentInChildren<Text>();
+        label.text = tower.name;
+    }
+
+    public void DeleteSelectedItem()
+    {
+        int selectedIndex = GetActiveToggleIndex();
+        DestoryToggle(selectedIndex);
+        DeleteSelectedTower(selectedIndex);
+    }
+
+    public void DestoryToggle(int selectedIndex)
+    {
+        Debug.Log(_toggle.Count);
+        Debug.Log("destroy");
+        if (selectedIndex != -1)
+        {
+            _toggle[selectedIndex].isOn = false;
+            Destroy(_toggle[selectedIndex].gameObject);
+            _toggle.RemoveAt(selectedIndex);
+            _towerManager.towerToSpawn = null;
+            return;
+        }
+    }
+
+    public void DeleteSelectedTower(int selectedIndex)
+    {
+        _tower.RemoveAt(selectedIndex);
+    }
+
+    public void SetToggleInteractable(bool interactable)
+    {
+        for (int i = 0; i < _toggle.Count; i++)
+        {
+            _toggle[i].interactable = interactable;
+        }
+    }
+
+    public bool isFull()
+    {
+        return _toggle.Count >= MAX_INVENTORY_CAPACITY;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+    public float maxHP;
+    public float currentHP;
+    public GameManager gameManager;
+    public Text hpText;
+    
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        currentHP = maxHP;
+        this.anim = transform.GetComponent<Animator>();
+        UpdateHpText(currentHP);
+
+
+    }
+
+    void Update()
+    {
+        if (currentHP <= 0) StartCoroutine(OnDie());
+    }
+
+    public void UpdateHpText(float hp) // 피격시 실행
+    {
+        if (hp < 0)
+        {
+            hp = 0;
+        }
+        hpText.text = string.Format("X {0}", hp);
+    }
+
+    public void StartGetHit(float hitDamage)
+    {
+        StartCoroutine(GetHitCoroutine(hitDamage));
+    }
+
+
+    public IEnumerator GetHitCoroutine(float damage)
+    {
+        currentHP -= damage;
+        UpdateHpText(currentHP);
+        anim.SetBool("isHit", true);
+        yield return new WaitForSeconds(0.65f);
+        anim.SetBool("isHit", false);
+
+
+    }
+
+
+    IEnumerator OnDie()
+    {
+        this.transform.GetComponent<BoxCollider>().enabled = false;
+        anim.SetBool("isHit", false);
+        anim.SetBool("isDead", true);
+        gameManager.isGameOver = true;
+        yield return null;
+        
+    }
+
+    
+   
+    
+}
