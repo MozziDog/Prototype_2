d1b8d8df41cb06ddb629d64255218919c45b0562
bbdlek <bbdlek@gmail.com>
2021-09-13 19:49:24 +0900

    Edit Earthquake
    Edit Earthquake

--- Assets/SkillSystem/SkillScript/Earthquake.cs
+++ Assets/SkillSystem/SkillScript/Earthquake.cs
@@ -2,35 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class Earthquake : MonoBehaviour
+public class Earthquake : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
 {
+
     [SerializeField] Button _button;
     [SerializeField] Image _cool_Img;
     [SerializeField] GameObject _cool_txt;
 
-    [SerializeField] float coolTime = 3f;
+    [SerializeField] float coolTime = 30f;
+    [SerializeField] float coolMaxTime = 30f;
     public bool isClicked = false;
-    [SerializeField] float leftTime = 3f;
+    [SerializeField] float leftTime = 0f;
 
-    [SerializeField] MoneyManager _moneyManager;
-    [SerializeField] GameObject _healEffect;
-    [SerializeField] GameObject _player;
+    [SerializeField] private float holdTime = 10f;
+    [SerializeField] private float _holdTime = 0f;
 
-    [SerializeField] int _moneyAmount;
-    [SerializeField] int _moneyUpgradeAmount;
-    [SerializeField] float _healAmount;
-    //[SerializeField] float _healUpgradeAmount;
-    [SerializeField] int _upgraded = 1;
+    [SerializeField] float _coolTime;
+
+    [SerializeField] EnemyManager _enemyManager;
+
 
     private void Start()
     {
-        _moneyAmount -= (_upgraded - 1) * _moneyUpgradeAmount;
-        //_healAmount += (_upgraded - 1) * _healUpgradeAmount;
-        _player = GameObject.Find("Player1");
-        _moneyManager = GameObject.Find("InGameShopManager").GetComponent<MoneyManager>();
+        _enemyManager = GameObject.Find("SpawnPointGroup").GetComponent<EnemyManager>();
     }
-
     private void Update()
     {
         if (isClicked)
@@ -55,6 +53,83 @@ public class Earthquake : MonoBehaviour
             }
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(leftTime == 0f)
+        {
+            isClicked = true;
+            StartCoroutine(CountDown());
+            StartCoroutine(EarthquakeSkill());
+            Invoke("OnLongPress", holdTime);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (leftTime == 0f)
+        {
+            StopAllCoroutines();
+            coolTime = coolMaxTime * _holdTime / holdTime;
+            Debug.Log(_holdTime);
+            StartCoolTime();
+            _holdTime = 0f;
+            CancelInvoke("OnLongPress");
+            for (int i = 0; i < _enemyManager.CurrentEnemyList.Count; i++)
+            {
+                _enemyManager.CurrentEnemyList[i].GetComponent<GroundEnemy>().agent.speed = 1f;
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (leftTime == 0f)
+        {
+            StopAllCoroutines();
+            CancelInvoke("OnLongPress");
+            for (int i = 0; i < _enemyManager.CurrentEnemyList.Count; i++)
+            {
+                _enemyManager.CurrentEnemyList[i].GetComponent<GroundEnemy>().agent.speed = 1f;
+            }
+        }
+        
+    }
+
+    private void OnLongPress()
+    {
+        StopAllCoroutines();
+        coolTime = coolMaxTime * _holdTime / holdTime;
+        Debug.Log(_holdTime);
+        StartCoolTime();
+        _holdTime = 0f;
+        CancelInvoke("OnLongPress");
+        for (int i = 0; i < _enemyManager.CurrentEnemyList.Count; i++)
+        {
+            _enemyManager.CurrentEnemyList[i].GetComponent<GroundEnemy>().agent.speed = 1f;
+        }
+    }
+
+    IEnumerator CountDown()
+    {
+        while (true)
+        {
+            _holdTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+    
+    IEnumerator EarthquakeSkill()
+    {
+        while (true)
+        {
+            for (int i = 0; i < _enemyManager.CurrentEnemyList.Count; i++)
+            {
+                _enemyManager.CurrentEnemyList[i].GetComponent<GroundEnemy>().agent.speed = 0f;
+            }
+            yield return null;
+        }
+    }
+
     public void StartCoolTime()
     {
         leftTime = coolTime;
@@ -64,13 +139,4 @@ public class Earthquake : MonoBehaviour
         _cool_txt.SetActive(true);
     }
 
-    public void HealSkill()
-    {
-        if (_moneyManager.GetLeftMoney() < _moneyAmount || _player.GetComponent<Player>().currentHP >= _player.GetComponent<Player>().maxHP)
-            return;
-        StartCoolTime();
-        _moneyManager.SpendMoney(_moneyAmount);
-        _player.GetComponent<Player>().getHealed(_healAmount);
-        Instantiate(_healEffect, _player.transform);
-    }
 }
