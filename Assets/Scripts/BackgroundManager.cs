b3cf74109a686537f92e55b41164cdf388b2b727
김태현 <th2252@gmail.com>
2021-09-16 03:49:50 +0900

    장애물 로딩 구현
    스테이지 별로 지정된 장애물을 불러오는 기능 추가

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BackgroundManager : MonoBehaviour
 {
     public GameObject[] backgroundPrefab;
+    public ChapterObstacle[] stageObstacle;
 
     // Start is called before the first frame update
     void Start()
@@ -12,6 +13,10 @@ public class BackgroundManager : MonoBehaviour
         if (backgroundPrefab.Length >= Global._chapter)
         {
             Instantiate(backgroundPrefab[Global._chapter - 1], Vector3.zero, Quaternion.identity);
+            if (stageObstacle[Global._chapter - 1].obstacles[Global._stage - 1] != null)
+            {
+                Instantiate(stageObstacle[Global._chapter - 1].obstacles[Global._stage - 1], Vector3.zero, Quaternion.identity);
+            }
         }
 
     }
@@ -21,4 +26,10 @@ public class BackgroundManager : MonoBehaviour
     {
 
     }
+}
+
+[System.Serializable]
+public class ChapterObstacle
+{
+    public GameObject[] obstacles;
 }
